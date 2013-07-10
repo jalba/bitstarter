03a92fcb56b81977a228c90c2695382b74a9b6c2
jalba <javierosvaldoalba@gmail.com>
2013-07-10 20:47:00 -0300

    back

--- web.js
+++ web.js
@@ -1,27 +1,12 @@
-//var express = require('express');
-  var fs  = require('fs');
-  var http = require('http');
-//var app = express.createServer(express.logger());
+var express = require('express');
 
+var app = express.createServer(express.logger());
 
-fs.readFile('index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(5000);
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-console.log("Listening on port 5000");
-
-//app.get('/', function(request, response) {
-  //response.sendfile(__dirname + '/index.html');
-//});
-
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-  //console.log("Listening on " + port);
-//});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
