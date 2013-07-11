53b09c2026dbb080eb3fed717089fee14dd08dda
jalba <javierosvaldoalba@gmail.com>
2013-07-10 21:13:10 -0300

    hello world

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
