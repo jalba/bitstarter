66ea373d68af4349c3168d971fad313165cffadc
Javier Alba <jalba@Javiers-MacBook-Pro.local>
2013-09-03 08:15:45 -0300

    handling images

--- web.js
+++ web.js
@@ -4,10 +4,13 @@ var app = express.createServer(express.logger());
 
 var fs = require('fs');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response) {
   var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
   response.send(buf.toString());
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
